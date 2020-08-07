eb88408bc4e3aeb5aebcea151de44c52e78ef965
funny0628 <610531115@qq.com>
2020-08-07 14:29:21 +0800

    获取当前经纬度的城市名称

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,22 @@
 //index.js
+import {getcityname} from '../../utils/city.js'
 //获取应用实例
 const app = getApp()
-
+// 引用百度地图微信小程序JSAPI模块 
+let bmap = require('../../libs/bmap-wx.js');
+let local = ''
 Page({
   data: {
     //图片基地址
-    daseUrl:'',
+    daseUrl: '',
     //轮播图列表
     imagelist: [],
     //租房小组
-    grouplist:[],
+    grouplist: [],
     //最新资讯
-    newslist:[],
+    newslist: [],
+    //城市名字
+    cityname: '',
     arealist: [{
         clas: 'iconfont icon-icon-home',
         text: "整租"
@@ -28,29 +33,71 @@ Page({
         clas: 'iconfont icon-chuzu',
         text: "去出租"
       },
-    ]
+    ],
+    //地图数据
+
   },
   //事件处理函
+  getcityname() {
+    var BMap = new bmap.BMapWX({
+      ak: 'x6QfgHjg6kdB0trGeIdu67Chzwxl2adT'
+    });
+
+    let success = (data) => {
+      this.setData({
+        cityname: data.currentWeather[0].currentCity
+      })
+      
+    }
+    let fail = function (data) {
+      console.log(data);
+    }
+    BMap.weather({
+      location: local,
+      success: success,
+      fail: fail
+    })
+  },
 
   onLoad: function (options) {
     this.setData({
-      baseUrl:wx.$baseurl
+      baseUrl: wx.$baseurl
     })
     this.getswiper();
     this.getgroup();
     this.getnews();
+    //获取当前位置的经纬度
+    // this.getLocation()
+    this.setData({
+      cityname:getcityname()
+    })
+  },
+
+  //获取当前的的位置
+  getLocation() {
+    let that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        local = res.longitude + ',' + res.latitude
+        //由当前位置的经纬度获取城市名字
+        // that.getcityname()
+      }
+    })
   },
 
   //请求轮播图数据
   async getswiper() {
     wx.$showToast()
-    let {data} = await wx.$http({
+    let {
+      data
+    } = await wx.$http({
       url: '/home/swiper',
       method: 'GET',
     })
-    if(data.status === 200 && data.description === '请求成功'){
+    if (data.status === 200 && data.description === '请求成功') {
       this.setData({
-        imagelist:data.body
+        imagelist: data.body
       })
       wx.$hideToast()
     }
@@ -58,13 +105,15 @@ Page({
   //请求租房小组数据
   async getgroup() {
     wx.$showToast()
-    let {data} = await wx.$http({
+    let {
+      data
+    } = await wx.$http({
       url: '/home/groups',
       method: 'GET',
     })
-    if(data.status === 200 && data.description === '请求成功'){
+    if (data.status === 200 && data.description === '请求成功') {
       this.setData({
-        grouplist:data.body
+        grouplist: data.body
       })
       wx.$hideToast()
     }
@@ -72,13 +121,15 @@ Page({
   //请求最新资讯数据
   async getnews() {
     wx.$showToast()
-    let {data} = await wx.$http({
+    let {
+      data
+    } = await wx.$http({
       url: '/home/news',
       method: 'GET',
     })
-    if(data.status === 200 && data.description === '请求成功'){
+    if (data.status === 200 && data.description === '请求成功') {
       this.setData({
-        newslist:data.body
+        newslist: data.body
       })
       wx.$hideToast()
     }
---
+++ utils/city.js
@@ -0,0 +1,44 @@
+// 引用百度地图微信小程序JSAPI模块 
+let bmap = require('../libs/bmap-wx.js');
+let local = '';
+let city = ""
+
+function getname() {
+  return wx.getStorageSync('CITY')
+}
+
+function setname(name) {
+  wx.setStorageSync('CITY', name)
+}
+
+export function getcityname() {
+  city = getname()
+  if (!city) {
+    //先拿到本地的经纬度
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        local = res.longitude + ',' + res.latitude
+      }
+    })
+    //实例化地图,地址解析
+    var BMap = new bmap.BMapWX({
+      ak: 'x6QfgHjg6kdB0trGeIdu67Chzwxl2adT'
+    });
+
+    let success = (data) => {
+      setname(data.currentWeather[0].currentCity)
+      city = data.currentWeather[0].currentCity
+    }
+    let fail = function (data) {
+      console.log(data);
+      return data
+    }
+    BMap.weather({
+      location: local,
+      success: success,
+      fail: fail
+    })
+  }
+  return city;
+}
